378347264cf08cbdf50b43df8dc1212f5ebac1c5
UNIZA\uniza.lin <unizalin@gmail.com>
2019-04-02 13:51:12 +0800

    add show.handlebars

--- app.js
+++ app.js
@@ -14,8 +14,19 @@ const restaurant = require('./restaurant.json');
 
 app.get('/', (req, res) => {
   // res.send('test');
+  //render 到指定的 handlebars 
+  console.log(restaurant)
   res.render('index', { restaurants: restaurant.results });
 });
+
+app.get('/restaurant/:store_id', (req, res) => {
+  console.log(req.params.store_id)
+  const store = restaurant.results.filter(store => store.id == req.params.store_id);
+  console.log(typeof (store));
+  console.log(typeof (restaurant));
+  console.log(this)
+  res.render('show', { restaurant: store[0] })
+})
 
 
 app.listen(port, () => {

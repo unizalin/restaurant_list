88ef836653fe06dfd20d3711ffcce7cc7c8d385f
UNIZA\uniza.lin <unizalin@gmail.com>
2019-04-02 12:06:37 +0800

    建立index handlebars 頁面，帶入json 資料，用each 跑出迴圈

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
-const app = express;
+const app = express();
 const port = 3000;
 
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-
 app.set('view engine', 'handlebars');
+//引入靜態檔案 js&css
+app.use(express.static("public"));
+
+//引入 json 
+const restaurant = require('./restaurant.json');
+
+
+app.get('/', (req, res) => {
+  // res.send('test');
+  res.render('index', { restaurants: restaurant.results });
+});
+
+
+app.listen(port, () => {
+  console.log(`${port}`);
+});
